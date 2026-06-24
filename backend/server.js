a68b632248c88b064190592b47cbee6ab329edf3
Shagun Gupta <your-email@example.com>
2026-06-25 01:09:07 +0530

    fix CORS for Vercel

--- backend/server.js
+++ backend/server.js
@@ -5,7 +5,12 @@ require('dotenv').config();
 
 const app = express();
 app.use(cors({
-  origin: ['http://127.0.0.1:5500', 'http://localhost:5500'],
+  origin: [
+    'http://127.0.0.1:5500', 
+    'http://localhost:5500',
+    'https://hostel-roommate-system.vercel.app',
+    'https://hostelroomatesystem.netlify.app'
+  ],
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
   credentials: true
 }));
